e349696657ee81fbe6ae1c44a3bd0dbc28c63b9a
pavel <pavelrazrushitel1ads@mail.ru>
2025-08-27 22:01:28 +0700

    Fixed not loading pictures of houses

--- scripts/selection-house.js
+++ scripts/selection-house.js
@@ -1,82 +1,82 @@
 const allHouses = [
-    {
-        id: 1,
-        title: 'LOFTHAUS R70',
-        price: 4465000,
-        area: 70,
-        img: 'https://topttedhbiu-dejlbfuh4uk.github.io/Layout_LoftHouse/images/item-1.jpg',
+  {
+    id: 1,
+    title: 'LOFTHAUS R70',
+    price: 4465000,
+    area: 70,
+    img: 'https://pahumbiu.github.io/Layout_LoftHouse/images/item-1.jpg',
 
-    },
-    {
-        id: 2,
-        title: 'LOFTHAUS R90',
-        price: 5115000,
-        area: 90,
-        img: 'https://topttedhbiu-dejlbfuh4uk.github.io/Layout_LoftHouse/images/item-2.jpg',
-    },
-    {
-        id: 3,
-        title: 'LOFTHAUS M102',
-        price: 6360000,
-        area: 102,
-        img: 'https://topttedhbiu-dejlbfuh4uk.github.io/Layout_LoftHouse/images/item-3.jpg',
-    },
-    {
-        id: 4,
-        title: 'LOFTHAUS M125',
-        price: 7300000,
-        area: 125,
-        img: 'https://topttedhbiu-dejlbfuh4uk.github.io/Layout_LoftHouse/images/item-4.jpg',
-    },
-    {
-        id: 5,
-        title: 'LOFTHAUS M130',
-        price: 7910000,
-        area: 130,
-        img: 'https://topttedhbiu-dejlbfuh4uk.github.io/Layout_LoftHouse/images/item-5.jpg',
-    },
-    {
-        id: 6,
-        title: 'LOFTHAUS С140',
-        price: 6970000,
-        area: 140,
-        img: 'https://topttedhbiu-dejlbfuh4uk.github.io/Layout_LoftHouse/images/item-6.jpg',
-    },
-    {
-        id: 7,
-        title: 'LOFTHAUS M150',
-        price: 8820000,
-        area: 150,
-        img: 'https://topttedhbiu-dejlbfuh4uk.github.io/Layout_LoftHouse/images/item-7.jpg',
-    },
-    {
-        id: 8,
-        title: 'LOFTHAUS S160',
-        price: 9120000,
-        area: 160,
-        img: 'https://topttedhbiu-dejlbfuh4uk.github.io/Layout_LoftHouse/images/item-8.jpg',
-    },
-    {
-        id: 9,
-        title: 'LOFTHAUS ONE 200',
-        price: 16740000,
-        area: 200,
-        img: 'https://topttedhbiu-dejlbfuh4uk.github.io/Layout_LoftHouse/images/item-9.jpg',
-    },
-    {
-        id: 10,
-        title: 'LOFTHAUS S207',
-        price: 11420000,
-        area: 207,
-        img: 'https://topttedhbiu-dejlbfuh4uk.github.io/Layout_LoftHouse/images/item-10.jpg',
-    },
-    {
-        id: 11,
-        title: 'LOFTHAUS К227',
-        price: 10850000,
-        area: 227,
-        img: 'https://topttedhbiu-dejlbfuh4uk.github.io/Layout_LoftHouse/images/item-11.jpg',
-    },
+  },
+  {
+    id: 2,
+    title: 'LOFTHAUS R90',
+    price: 5115000,
+    area: 90,
+    img: 'https://pahumbiu.github.io/Layout_LoftHouse/images/item-2.jpg',
+  },
+  {
+    id: 3,
+    title: 'LOFTHAUS M102',
+    price: 6360000,
+    area: 102,
+    img: 'https://pahumbiu.github.io/Layout_LoftHouse/images/item-3.jpg',
+  },
+  {
+    id: 4,
+    title: 'LOFTHAUS M125',
+    price: 7300000,
+    area: 125,
+    img: 'https://pahumbiu.github.io/Layout_LoftHouse/images/item-4.jpg',
+  },
+  {
+    id: 5,
+    title: 'LOFTHAUS M130',
+    price: 7910000,
+    area: 130,
+    img: 'https://pahumbiu.github.io/Layout_LoftHouse/images/item-5.jpg',
+  },
+  {
+    id: 6,
+    title: 'LOFTHAUS С140',
+    price: 6970000,
+    area: 140,
+    img: 'https://pahumbiu.github.io/Layout_LoftHouse/images/item-6.jpg',
+  },
+  {
+    id: 7,
+    title: 'LOFTHAUS M150',
+    price: 8820000,
+    area: 150,
+    img: 'https://pahumbiu.github.io/Layout_LoftHouse/images/item-7.jpg',
+  },
+  {
+    id: 8,
+    title: 'LOFTHAUS S160',
+    price: 9120000,
+    area: 160,
+    img: 'https://pahumbiu.github.io/Layout_LoftHouse/images/item-8.jpg',
+  },
+  {
+    id: 9,
+    title: 'LOFTHAUS ONE 200',
+    price: 16740000,
+    area: 200,
+    img: 'https://pahumbiu.github.io/Layout_LoftHouse/images/item-9.jpg',
+  },
+  {
+    id: 10,
+    title: 'LOFTHAUS S207',
+    price: 11420000,
+    area: 207,
+    img: 'https://pahumbiu.github.io/Layout_LoftHouse/images/item-10.jpg',
+  },
+  {
+    id: 11,
+    title: 'LOFTHAUS К227',
+    price: 10850000,
+    area: 227,
+    img: 'https://pahumbiu.github.io/Layout_LoftHouse/images/item-11.jpg',
+  },
 ];
 
 const containerBtnApplyEl = document.querySelector('.container-btn-apply');
@@ -88,29 +88,27 @@ const containerItemEls = document.querySelector('.container-items');
 const noResultsMessageEl = document.querySelector('.no-results-message');
 
 const renderHouse = (house, index) => {
-    const houseTemplate = templateEl.content.firstElementChild.cloneNode(true);
+  const houseTemplate = templateEl.content.firstElementChild.cloneNode(true);
 
-    if (index >= 9) {
-        houseTemplate.classList.add('hidden');
-    }
+  if (index >= 9) houseTemplate.classList.add('hidden');
 
-    const imgEl = houseTemplate.querySelector('img');
-    imgEl.src = house.img;
+  const imgEl = houseTemplate.querySelector('img');
+  imgEl.src = house.img;
 
-    const modelHouseEl = houseTemplate.querySelector('.model-house');
-    modelHouseEl.innerText = house.title;
+  const modelHouseEl = houseTemplate.querySelector('.model-house');
+  modelHouseEl.innerText = house.title;
 
-    const priceHouseEl = houseTemplate.querySelector('.price-house');
-    priceHouseEl.innerText = `${new Intl.NumberFormat('ru-RU', {}).format(house.price).replace(/\s/g, '.')} ₽`;
+  const priceHouseEl = houseTemplate.querySelector('.price-house');
+  priceHouseEl.innerText = `${new Intl.NumberFormat('ru-RU', {}).format(house.price).replace(/\s/g, '.')} ₽`;
 
-    const sizeHouseEl = houseTemplate.querySelector('.size-house');
-    sizeHouseEl.innerText = `${house.area}м2`;
+  const sizeHouseEl = houseTemplate.querySelector('.size-house');
+  sizeHouseEl.innerText = `${house.area}м2`;
 
-    containerItemEls.append(houseTemplate);
+  containerItemEls.append(houseTemplate);
 };
 
 allHouses.forEach((house, index) => {
-    renderHouse(house, index);
+  renderHouse(house, index);
 });
 
 const showAllHousesEl = document.querySelector('.show-all-houses');
@@ -119,101 +117,102 @@ const containerShowAllHousesEl = document.querySelector('.container-show-all-hou
 const showAllHousesWrapperEl = document.querySelector('.show-all-houses-wrapper');
 
 showAllHousesEl.addEventListener('click', () => {
-    hiddenHouseEls.forEach(hiddenHouse => {
-        hiddenHouse.classList.remove('hidden');
-    });
-    containerShowAllHousesEl.classList.remove('container-show-all-houses');
-    showAllHousesWrapperEl.classList.remove('show-all-houses-wrapper');
-    showAllHousesEl.classList.remove('show-all-houses');
-    showAllHousesEl.innerText = '';
+  hiddenHouseEls.forEach(hiddenHouse => {
+    hiddenHouse.classList.remove('hidden');
+  });
+  containerShowAllHousesEl.classList.remove('container-show-all-houses');
+  showAllHousesWrapperEl.classList.remove('show-all-houses-wrapper');
+  showAllHousesEl.classList.remove('show-all-houses');
+  showAllHousesEl.innerText = '';
 });
 
 const clearContainerItemEls = () => {
-    [...containerItemEls.children].forEach(el => el.remove());
+  [...containerItemEls.children].forEach(el => el.remove());
 };
 
 const getHouses = (filters) => {
-    const response = allHouses.filter(house => {
-        for (const filterKey in filters) {
-            const filterVal = filters[filterKey];
-            switch (filterKey) {
-                case 'price': {
-                    if (house.price < filterVal.from) return false;
-                    if (house.price > filterVal.to) return false;
-                    break;
-                }
-                case 'area': {
-                    if (house.area < filterVal.from) return false;
-                    if (house.area > filterVal.to) return false;
-                    break;
-                }
-            }
+  const response = allHouses.filter(house => {
+    for (const filterKey in filters) {
+      const filterVal = filters[filterKey];
+      switch (filterKey) {
+        case 'price': {
+          if (house.price < filterVal.from) return false;
+          if (house.price > filterVal.to) return false;
+          break;
         }
-        return true;
-    });
-    return response.length ? response : null;
+        case 'area': {
+          if (house.area < filterVal.from) return false;
+          if (house.area > filterVal.to) return false;
+          break;
+        }
+      }
+    }
+    return true;
+  });
+  return response.length ? response : null;
 };
 
 const isValidFilterInputEl = (filterInputEl) => {
-    return !!filterInputEl.value || filterInputEl.value === 0;
+  return !!filterInputEl.value || filterInputEl.value === 0;
 };
 
 const validateFilterInputEl = (filterInputEl) => {
-    const isValid = isValidFilterInputEl(filterInputEl);
-    filterInputEl.filterInvalidMsgEls.classList.toggle('hidden-filter-invalid-msg', isValid);
-    return isValid;
+  const isValid = isValidFilterInputEl(filterInputEl);
+  filterInputEl.filterInvalidMsgEls.classList.toggle('hidden-filter-invalid-msg', isValid);
+  return isValid;
 };
 
 filterInputEls.forEach((filterInputEl, index) => {
-    filterInputEl.filterInvalidMsgEls = filterInvalidMsgEls[index];
+  filterInputEl.filterInvalidMsgEls = filterInvalidMsgEls[index];
 
-    filterInputEl.addEventListener('input', () => {
-        validateFilterInputEl(filterInputEl);
-    });
+  filterInputEl.addEventListener('input', () => {
+    validateFilterInputEl(filterInputEl);
+  });
 });
 
 const applyElClickHandler = () => {
 
-    filterInputEls.forEach(filterInputEl => {
-        if (!validateFilterInputEl(filterInputEl)) {
-            return false;
-        }
-    });
-
-    const filters = {};
-    filterInputEls.forEach(filterInputEl => {
-        const [filterKey, filterSubKey] = filterInputEl.id.split('-');
-        const filter = filters[filterKey] = filters[filterKey] || {};
-        filter[filterSubKey] = filterInputEl.value;
-    });
-
-    const houses = getHouses(filters);
-
-    clearContainerItemEls();
-
-    containerShowAllHousesEl.classList.remove('container-show-all-houses');
-    showAllHousesWrapperEl.classList.remove('show-all-houses-wrapper');
-    showAllHousesEl.classList.remove('show-all-houses');
-    showAllHousesEl.innerText = '';
-
-    if (!houses) {
-        noResultsMessageEl.classList.remove('hidden-message');
-        return false;
+  filterInputEls.forEach(filterInputEl => {
+    if (!validateFilterInputEl(filterInputEl)) {
+      return false;
     }
+  });
 
-    noResultsMessageEl.classList.add('hidden-message');
-    houses.forEach(house => {
-        renderHouse(house);
-    });
+  const filters = {};
+
+  filterInputEls.forEach(filterInputEl => {
+    const [filterKey, filterSubKey] = filterInputEl.id.split('-');
+    const filter = filters[filterKey] = filters[filterKey] || {};
+    filter[filterSubKey] = filterInputEl.value;
+  });
+
+  const houses = getHouses(filters);
+
+  clearContainerItemEls();
+
+  containerShowAllHousesEl.classList.remove('container-show-all-houses');
+  showAllHousesWrapperEl.classList.remove('show-all-houses-wrapper');
+  showAllHousesEl.classList.remove('show-all-houses');
+  showAllHousesEl.innerText = '';
+
+  if (!houses) {
+    noResultsMessageEl.classList.remove('hidden-message');
+    return false;
+  }
+
+  noResultsMessageEl.classList.add('hidden-message');
+  houses.forEach(house => {
+    renderHouse(house);
+  });
 };
 
 containerBtnApplyEl.addEventListener('click', (e) => {
-    containerBtnResetEl.classList.remove('hidden-btn-reset');
-    applyElClickHandler(e);
+  containerBtnResetEl.classList.remove('hidden-btn-reset');
+  applyElClickHandler(e);
 });
 
 const resetFunction = () => {
-    location.reload();
+  location.reload();
 };
 containerBtnResetEl.addEventListener('click', resetFunction);
 
@@ -222,9 +221,10 @@ const svgIconCloseModalWindowSelectionHouseEl = document.querySelector('.svg-ico
 const containerBtnParamsEl = document.querySelector('.container-btn-params');
 
 containerBtnParamsEl.addEventListener('click', () => {
-    containerModalWindowSelectionHouseEl.classList.toggle('hidden-modal-window-selection-house');
+  containerModalWindowSelectionHouseEl.classList.toggle('hidden-modal-window-selection-house');
 });
 
 svgIconCloseModalWindowSelectionHouseEl.addEventListener('click', () => {
-    containerModalWindowSelectionHouseEl.classList.add('hidden-modal-window-selection-house');e
+  containerModalWindowSelectionHouseEl.classList.add('hidden-modal-window-selection-house');
+  e
 });
